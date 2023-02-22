a9f4c7d169dae531485ddba988161f122448a464
Allan2Fernandes <allan2fernandes@hotmail.com>
2023-02-22 15:42:14 +0100

    Modified the file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from "./Components/App.js"
 import "./Components/Style.css"
 
 
 
-ReactDOM.render(<App />,document.getElementById("root"))
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+)
